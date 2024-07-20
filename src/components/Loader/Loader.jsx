d58d4ae01632234166026c47e094c78c70f0c689
devromarc <rmrcslvdr@gmail.com>
2024-07-20 16:49:25 +0800

    refactor to functional components

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
 import { FallingLines } from 'react-loader-spinner';
 import styles from './Loader.module.css';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className={styles.Loader}>
-        <div className={styles.loaderBox}>
-          <FallingLines color="#F6E6CB" height={200} width={200} />
-        </div>
+const Loader = () => {
+  return (
+    <div className={styles.Loader}>
+      <div className={styles.loaderBox}>
+        <FallingLines color="#F6E6CB" height={200} width={200} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Loader;
